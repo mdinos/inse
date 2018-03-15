0da8d4a8d876eca883924a7559e5141613db59c4
hashimh <36477128+hashimh@users.noreply.github.com>
2018-03-15 21:41:27 +0000

    added create event validation
    hopefully this has fixed that folder thing

--- webpages/js/main.js
+++ webpages/js/main.js
@@ -63,34 +63,6 @@ async function populateMain(googleUser) {
   // populate me!
 }
 
-async function deliver(page) {
-    page += ".html";
-    const token = localStorage.getItem("id_token");
-    let auth2 = gapi.auth2.getAuthInstance();
-    
-    const fetchOptions = {
-        credentials: 'same-origin',
-        method: 'GET',
-        headers: { 'Authorization': 'Bearer ' + token }
-    };
-    
-    let url = '/api/deliver';
-    
-    url += '?page=' + page;
-    
-    const response = await fetch(url, fetchOptions);
-    
-    if (!response.ok) {
-        console.log("fetch response for api/deliver has failed");
-        return
-    }
-    let innerhtml = await response.text();
-    console.log("innerhtml:")
-    console.log(innerhtml);
-    document.documentElement.innerHTML = innerhtml;
-    
-}
-
 async function signOut() {
   console.log("entered function");
   const token = localStorage.getItem("id_token");
@@ -110,6 +82,7 @@ async function signOut() {
   }
 
   let innerhtml = await response.text();
+  console.log(innerhtml);
   document.documentElement.innerHTML = innerhtml;
 
   window.location.reload();
@@ -125,57 +98,104 @@ async function makeEvent() {
     let testBalance = parseInt(document.getElementById('totalSpent').value);
     let _emails = document.getElementById('emaillist').children;
     let emails = [];
-    
-    for (i = 0; i < _emails.length; i++) {
-        emails.push(_emails[i].innerHTML);
+
+    let eventStatus = validateEntries();
+    console.log("Status of this event: " + eventStatus);
+
+    if (eventStatus == true) {
+      for (i = 0; i < _emails.length; i++) {
+          emails.push(_emails[i].innerHTML);
+      }
+
+      let url = '/api/makeevent';
+      const _token = localStorage.getItem("id_token");
+
+      url += '?eventName=' + eventName;
+      url += '&eventDesc=' + eventDescription;
+      url += '&testBalance=' + testBalance;
+      url += '&emails=' + emails;
+
+      const _fetchOptions = {
+          credentials: 'same-origin',
+          method: 'POST',
+          headers: { 'Authorization': 'Bearer ' + _token },
+      };
+
+      const response = await fetch(url, _fetchOptions);
+
+      if (!response.ok) {
+          console.log("fetch for /api/makeevent' has failed");
+          return;
+      }
+      alert("Database updated!");
+    } else {
+      alert("Database not updated!");
+      return;
     }
-    
-    let url = 'api/makeevent';
-    const _token = localStorage.getItem("id_token");
-    
-    url += '?eventName=' + eventName;
-    url += '&eventDesc=' + eventDescription;
-    url += '&testBalance=' + testBalance;
-    url += '&emails=' + emails;
-    
-    const fetchOptions = {
-        credentials: 'same-origin',
-        method: 'POST',
-        headers: { 'Authorization': 'Bearer ' + _token },
-    };
-    
-    const response = await fetch(url, fetchOptions);
-    
-    if (!response.ok) {
-        console.log("fetch for /api/makeevent' has failed");
-        return;
-    }
+
 }
 
 // function to add another email to event, makes current emails appear in list below input box
 function addEmail() {
     let email = document.getElementById('emailin');
     let container = document.getElementById('emaillist');
-    
-    // validate email input
+
+    // validate email input - DONE
     // if email in database
     // then proceed
     // else send invite
-    
-    let displayEmail = document.createElement('li');
-    container.appendChild(displayEmail);
-    displayEmail.innerHTML = email.value;
-    email.value = "";
-    
-    let item = container.children;
-    
-    if (item[0].innerHTML == "No emails to display") {
-        container.removeChild(item[0]);
+
+    let validationStatus = validateEmail (email.value);
+
+    if (validationStatus == true) {
+      let displayEmail = document.createElement('li');
+      container.appendChild(displayEmail);
+      displayEmail.innerHTML = email.value;
+      email.value = "";
+
+      let item = container.children;
+
+      if (item[0].innerHTML == "No emails to display") {
+          container.removeChild(item[0]);
+      }
+      return false;
+    } else {
+      return false;
     }
-    
-    return false;
+
+
+}
+
+function validateEmail(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(String(email).toLowerCase());
 }
 
 function validateEntries() {
-    return false;
+  let isValid = true;
+
+  let _emails = document.getElementById('emaillist').children;
+  let eventName = document.getElementById('eventName').value;
+  let testBalance = parseInt(document.getElementById('totalSpent').value);
+
+  // note:
+  // find replacement for "_emails.length < 2", so that an event is able
+  // to have 1 email (instead of rn forced to have 2).
+
+  if ( _emails.length < 2) {
+    alert("Please enter user emails for this event!");
+    isValid = false;
+  }
+
+  if (eventName.length < 1) {
+    alert("Please enter a name for this event!");
+    isValid = false;
+  }
+
+  if (testBalance < 1) {
+    alert("Please enter the cost of this event!");
+    isValid = false;
+  }
+  console.log("validity before being sent: " + isValid);
+  return isValid;
 }
