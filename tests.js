5e93031331fe7544a079e42c44b0fb43f029c796
shivam <up744280@myport.ac.uk>
2018-03-16 16:21:10 +0000

    Updated tests.js
    Added testing for validateEmail() function

--- tests.js
+++ tests.js
@@ -8,3 +8,72 @@ QUnit.test('isEven()', function(assert) {
     assert.ok(!isEven(-3), ' -3 is odd');
     assert.ok(isEven(-10), ' -10 is even');
 });
+
+// Testing the page delivery system for pages after logon
+QUnit.module("Testing validation of entries in makeEvent, validateEmail, and validateEntries");
+QUnit.test('validateEmail()', function(assert) {
+   let emails = [
+       {  
+           "email": "m@b.com",
+           "ok": true,
+           "response": "Valid email address, should return true"
+       },
+       {
+           "email": "c@@g.com",
+           "ok": false,
+           "response": "Two @ symbols, should return false"
+       },
+       {
+           "email": "BBB.com",
+           "ok": false,
+           "response": "Not an email address, should return false"
+       },
+       {
+           "email": "Just words",
+           "ok": false,
+           "response": "Not an email address, should return false"
+       },
+       {
+           "email": "m@.com",
+           "ok": false,
+           "response": "No site address, should return false"
+       },
+       {
+           "email": "",
+           "ok": false,
+           "response": "Empty string should return false"
+       },
+       {
+           "email": 12345,
+           "ok": false,
+           "response": "Integer input should return false"
+       },
+       {
+           "email": "@g.com",
+           "ok": false,
+           "response": "No address at site, should return false"
+       },
+       {
+           "email": true,
+           "ok": false,
+           "response": "Boolean input, should return false"
+       },
+       {
+           "email": "m@g.",
+           "ok": false,
+           "response": "No site address, should return false"
+       },
+       {
+           "email": "email.with.dots@gmail.com",
+           "ok": true,
+           "response": "Should return true, dots should be ok"
+       }
+       ]
+   for (let i = 0; i < emails.length; i++) {
+       let funcCheck = "";
+       if (emails[i].ok == false)
+           funcCheck += "!"
+       funcCheck += "validateEmail(emails[i].email)";
+       assert.ok(funcCheck, emails[i].response + ". Email was: " + emails[i].email);
+   }
+});
