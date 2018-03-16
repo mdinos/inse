222aad7f4ec98e16f7f00232f241064896cb79a9
UP781330 <36160136+UP781330@users.noreply.github.com>
2018-03-16 16:28:06 +0000

    sql

---
+++ database/db.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE IF NOT EXISTS splitmatedb;
+
+CREATE TABLE 'user' (
+  'email' VARCHAR(64) PRIMARY KEY,
+  'fname' VARCHAR(40) NOT NULL,
+  'lname' VARCHAR(40) NOT NULL,
+  'admin' TINYINT(1),
+);
+
+
+CREATE TABLE 'group'(
+  'groupID' int(9) PRIMARY KEY auto_increment,
+  'groupName' VARCHAR(40),
+  'email' VARCHAR(64),
+);
+
+ALTER TABLE 'group' ADD CONSTRAINT 'FK_UserGroup' FOREIGN KEY ('email') REFERENCES 'user'('email');
